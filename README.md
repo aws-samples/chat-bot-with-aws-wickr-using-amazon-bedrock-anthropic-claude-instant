4d5011d4608569cdf665c0536374c3f74d9ee216
Charles Chowdhury-Hanscombe <charlcch@amazon.com>
2023-11-09 09:13:38 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,17 +1,63 @@
-## My Project
+# Chat-bot with AWS Wickr using Amazon Bedrock Anthropic Claude Instant v1
 
-TODO: Fill this README out!
+This sample will allow you to deploy a Generative AI chat-bot to your AWS Wickr network. It uses Amazon Bedrock and the Anthropic Claude Instant model for Q&A functionality. 
 
-Be sure to:
+## Prerequisites and limitations
 
-* Change the title in this README
-* Edit your repository description on GitHub
+## Prerequisites
 
-## Security
+- Access to Amazon Bedrock, and the **anthropic.claude-instant-v1** model enabled 
+- An existing AWS Wickr bot username and password
+- A supported host with Docker CE installed. This repo was tested on Ubuntu 22.04
+- IAM credentials configured on the host in the `~/.aws/config` and `~/.aws/credentials` file (see [here](https://docs.aws.amazon.com/cli/latest/reference/configure/) for details). The user must have the following IAM policy at a minimum (update with your Bedrock region):
+```
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "bedrock:InvokeModel"
+            ],
+            "Resource": [
+                "arn:aws:bedrock:<region>::foundation-model/anthropic.claude-instant-v1"
+            ]
+        }
+    ]
+}
+```
 
-See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more information.
+## Limitations
+
+For privacy reasons, this bot is 'intentionally forgetful', in that it is not aware of your previous questions or answers. This is because we do not wish to store your AWS Wickr conversations within AWS.
+
+## Installation
+
+1. Clone this repo and then enter the directory.
+2. Confirm you are within the repo directory, then zip and compress the contents: `tar czvf software.tar.gz *`. This will leave you with a file called `software.tar.gz`
+3. Create the bot folder: `sudo mkdir /opt/WickrIO`
+4. Copy this file to /opt/WickrIO: `sudo cp software.tar.gz /opt/WickrIO/`
+5. Start the WickrIO container with the following command: `docker run -v /home/ubuntu/.aws:/home/wickriouser/.aws -v /opt/WickrIO:/opt/WickrIO --restart=always --name="bedrock-claude_llm_bot" -ti public.ecr.aws/x3s2s6k3/wickrio/bot-cloud:latest`
+6. You will now be at the command line within the bot.
+
+## Configuration
+
+1. Select `yes` if you wish see welcome message on startup
+2. Enter `add` when prompted to enter a command
+3. Enter the bot username and password when prompted
+4. Select `yes` to auto-login
+5. When asked to **Enter the bot configuration to use**, enter `import`
+6. When asked for the location, enter `/opt/WickrIO`
+7. Enter `bedrock-claude-llm_bot` for the integration name
+
+The bot will now install.
+
+8. Enter your Amazon Bedrock region and credential profile when asked
+9. When you see **Successfully added record to the database!**, enter `start` and then provide the password when prompted
+10. Type `list` to see the status of the bot, it should say **Running** after a few seconds
+11. Send a message to the bot!
 
 ## License
 
 This library is licensed under the MIT-0 License. See the LICENSE file.
-
---
+++ bedrock-claude-llm_bot.js
@@ -0,0 +1,153 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+const WickrIOAPI = require('wickrio_addon');
+const WickrIOBotAPI = require('wickrio-bot-api');
+const util = require('util')
+const logger = require('wickrio-bot-api').logger
+const { fromIni } = require('@aws-sdk/credential-providers');
+const { BedrockRuntimeClient, InvokeModelCommand } = require('@aws-sdk/client-bedrock-runtime')
+
+console.log = function () {
+  logger.info(util.format.apply(null, arguments))
+}
+console.error = function () {
+  logger.error(util.format.apply(null, arguments))
+}
+
+var fs = require('fs');
+var path = require('path');
+
+module.exports = WickrIOAPI;
+process.stdin.resume(); // so the program will not close instantly
+var bot;
+
+
+async function exitHandler(options, err) {
+  try {
+    var closed = await bot.close();
+    console.log(closed);
+    if (err) {
+      console.log("Exit Error:", err);
+      process.exit();
+    }
+    if (options.exit) {
+      process.exit();
+    } else if (options.pid) {
+      process.kill(process.pid);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+//catches ctrl+c and stop.sh events
+process.on('SIGINT', exitHandler.bind(null, {
+  exit: true
+}));
+
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', exitHandler.bind(null, {
+  pid: true
+}));
+process.on('SIGUSR2', exitHandler.bind(null, {
+  pid: true
+}));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {
+  exit: true
+}));
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/////////////////////////////////////
+
+// Respond to user's input.
+async function createResponse(message, vGroupID) {
+  // setup the clients
+  bot.processesJsonToProcessEnv()
+  var tokens = JSON.parse(process.env.tokens)
+  const REGION = tokens.AWS_REGION.value
+  const profileName = tokens.AWS_PROFILENAME.value
+
+  console.log('entered createResponse')
+
+  const client = new BedrockRuntimeClient({
+    region: REGION,
+    credentials: fromIni({profile: profileName,}),
+  });
+
+  const aiModelId = 'anthropic.claude-instant-v1';
+  
+  const invokeModelParams = {
+    body: JSON.stringify({
+        prompt: "\n\nHuman: " + message + "\n\nAssistant:",
+        max_tokens_to_sample: 200
+        // temperature: 0.5
+    }),
+    modelId: aiModelId,
+    accept: 'application/json',
+    contentType: 'application/json'
+};
+  console.log('this was the request', invokeModelParams)
+
+  const command = new InvokeModelCommand(invokeModelParams);
+  const response = await client.send(command);
+
+  // Extract AI-generated text from the response
+  const aiResponseJson = JSON.parse(
+    new TextDecoder().decode(response.body)
+  );
+  const output =  aiResponseJson;
+  console.log('this was the output ', response);
+
+  WickrIOAPI.cmdSendRoomMessage(vGroupID, output.completion.trim());
+  console.log('this was the message ', convertedMessage);
+  
+}
+
+async function main() { // entry point
+  logger.info('entering main')
+  try {
+    var status;
+    if (process.argv[2] === undefined) {
+      var bot_username = fs.readFileSync('client_bot_username.txt', 'utf-8');
+      bot_username = bot_username.trim();
+      bot = new WickrIOBotAPI.WickrIOBot();
+      status = await bot.start(bot_username)
+    } else {
+      bot = new WickrIOBotAPI.WickrIOBot();
+      status = await bot.start(process.argv[2])
+    }
+    if (!status) {
+      exitHandler(null, {
+        exit: true,
+        reason: 'Client not able to start'
+      });
+    }
+
+    await bot.startListening(listen); // passes a callback function that will receive incoming messages into the bot client
+  } catch (err) {
+    logger.error(err);
+  }
+}
+
+async function listen(rMessage) { // starts a listener. Message payload accessible as 'message' 
+  logger.info('entering listen')
+  rMessage = JSON.parse(rMessage);
+  var sender = rMessage.sender;
+  var vGroupID = rMessage.vgroupid;
+  var userArr = [];
+  userArr.push(sender);
+  if (rMessage.message) {
+    var request = rMessage.message;
+
+    console.log('calling createResponse()')
+    await createResponse(request, vGroupID)
+  }
+}
+
+main();
